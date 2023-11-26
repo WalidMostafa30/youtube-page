b592e1ca253ec5cb22852b6a795d42164ac752c8
walidmostafa10 <111182017+walidmostafa10@users.noreply.github.com>
2023-11-26 16:42:17 +0200

    second commit

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,4 @@
+// #################### dark mode code #####################
 let toggleBtn = document.querySelector(".dark-mode");
 let moonBtn = document.querySelector(".dark-mode i");
 let body = document.querySelector("body");
@@ -19,4 +20,90 @@ toggleBtn.addEventListener("click", () => {
     moon.style.display = "block";
     sun.style.display = "none";
   }
+});
+// #################### dynamic code #####################
+let videoContainer = document.querySelector(".videos-container");
+let homeContainer = document.querySelector(".home-container");
+let youContainer = document.querySelector(".you-container");
+let subContainer = document.querySelector(".Subscription-container");
+let miniSideBar = document.querySelector(".mini-side-bar");
+let nav = document.querySelector("nav ul");
+function getVideo() {
+  fetch("../data/data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      data.video.map((e) => {
+        videoContainer.innerHTML += `<div class="video-card">
+              <a href="${e.src}" class="video-img">
+                <img
+                  src="${e.img}"
+                  alt="img"
+                  width="100%"
+                  />
+                  <span class="video-time">${e.VideoDuration}</span>
+              </a>
+              <a href="${e.src}" class="video-details">
+              <img src="${e.channelImg}" class="channel-img"/>
+              <div class="video-info">
+                  <h3 class="video-title">
+                    ${e.title}
+                    </h3>
+                  <span class="channel-name">
+                  ${e.channelName} <i class="fa-solid fa-circle-check"></i>
+                  </span>
+                  <p class="date-views">${e.publishDate} ago . ${e.views} views</p>
+                  </div>
+                  </a>
+                  </div>`;
+      });
+      data.home.map((e) => {
+        homeContainer.innerHTML += `<div class="big-side-li">
+                <h4>${e.title}</h4>
+                <i class="${e.icon}"></i>
+              </div>`;
+      });
+      data.you.map((e) => {
+        youContainer.innerHTML += `<div class="big-side-li">
+                <h4>${e.title}</h4>
+                <i class="${e.icon}"></i>
+              </div>`;
+      });
+      data.Subscriptions.map((e) => {
+        subContainer.innerHTML += `<div class="big-side-li">
+                <h4>${e.title}</h4>
+                <i class="${e.icon}"></i>
+              </div>`;
+      });
+      data.miniSideBar.map((e) => {
+        miniSideBar.innerHTML += `<div class="mini-side-li">
+            <i class="${e.icon}"></i>
+            <p>${e.title}</p>
+          </div>`;
+      });
+      data.navLi.map((e) => {
+        nav.innerHTML += `<li>${e.title}</li>`;
+      });
+    });
+}
+getVideo();
+// #################### toggle side bar code #####################
+let bars = document.querySelector(".bars-icon");
+let hiddenContainer = document.querySelector(".hidden-container");
+bars.addEventListener("click", () => {
+  hiddenContainer.classList.toggle("open");
+});
+document.addEventListener("click", (e) => {
+  if (e.target === hiddenContainer) {
+    hiddenContainer.classList.remove("open");
+  }
+});
+// #################### toggle search code #####################
+let back = document.querySelector(".back-icon");
+let srch = document.querySelector(".srch-clone");
+let searchContainer = document.querySelector(".search-container");
+srch.addEventListener("click", () => {
+  searchContainer.classList.add("open");
+});
+back.addEventListener("click", () => {
+  searchContainer.classList.remove("open");
 });
