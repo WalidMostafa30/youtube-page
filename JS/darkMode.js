d4f5c6d584a516b0020506a3e7e1f17400534bec
walidmostafa10 <111182017+walidmostafa10@users.noreply.github.com>
2023-11-30 22:46:01 +0200

    5 commit

--- JS/darkMode.js
+++ JS/darkMode.js
@@ -1,19 +1,28 @@
 // #################### dark mode code #####################
 let toggleBtn = document.querySelector(".dark-mode");
-let moonBtn = document.querySelector(".dark-mode i");
 let body = document.querySelector("body");
 let darkMode = false;
+let theme;
 let YouTube = document.querySelector(".logo-img img");
 let moon = document.querySelector(".fa-moon");
 toggleBtn.addEventListener("click", () => {
-  darkMode = !darkMode;
-  if (darkMode) {
-    body.classList.add("dark");
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
     YouTube.src = "./images/YouTubeWhite.svg";
+    moon.classList.remove("fa-moon");
+    moon.classList.add("fa-sun");
+    theme = "dark";
   } else {
-    body.classList.remove("dark");
     YouTube.src = "./images/YouTube.svg";
+    moon.classList.add("fa-moon");
+    moon.classList.remove("fa-sun");
+    theme = "light";
   }
-  moon.classList.toggle("fa-moon");
-  moon.classList.toggle("fa-sun");
+  localStorage.setItem("page-theme", theme);
 });
+if (localStorage.getItem("page-theme") === "dark") {
+  body.classList.add("dark");
+  YouTube.src = "./images/YouTubeWhite.svg";
+  moon.classList.remove("fa-moon");
+  moon.classList.add("fa-sun");
+}
